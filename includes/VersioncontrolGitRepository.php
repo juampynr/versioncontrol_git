351106517246c691dca25f1062dbc5d7108fbe6b
Sam Boyer <drupal@samboyer.org>
2010-08-11 15:42:46 +0000

    Updates pursuant to chasing the entities refactor in VCAPI. (Damn, committed the last to the wrong branch!)
    Squashed commit of the following:
    
    commit 4aa259897846babedb9d7af03bf01b4c7fcd5a89
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 11 08:39:08 2010 -0700
    
        Final batch of updates pursuant to the entity refactor.
    
    commit c2fcb0a54fbbe7963919c6cd26a8ca3e8b364856
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Fri Aug 6 12:45:07 2010 -0700
    
        Incremental checkin of the massive updates pursuant to the entities &
        dbtng refactor.
    
    commit bd1dc9a9fb938395d764981e029cbecab215b658
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Fri Aug 6 12:42:50 2010 -0700
    
        Added methods for fetching tags and commits from git.
    
    commit 192dd825ea6a415260f1c53e423aec1a45f8872b
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Fri Aug 6 12:41:39 2010 -0700
    
        Changed iterating logic to accommodate the weird expectation that
        next()/while must be used.
    
    commit d40cc946de18a3f3448d24a8aa4829a74b663d86
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 4 17:01:06 2010 -0700
    
        The key-setting stuff was removed from vcapi, so reintroduced the
        branches array rekeying logic.
    
    commit 85a6144137f6dd62ba677f0099044af3a2be5cd8
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 4 16:19:28 2010 -0700
    
        Initial work moving to entities; tidied up the calls and moved logic
        into the repository class.
    
    commit 36611b33b397ac7b2e26c40866f632217869de08
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 4 16:18:33 2010 -0700
    
        Added another trim() call just for safety.
    
    commit 05d4d968d15761c7b12d4a8de9cecafbcd8c2d7f
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 4 16:15:32 2010 -0700
    
        Move to the procedural repository loading syntax.
    
    commit 177c19a1f1ade71e3547d5596152a97cf2bcf211
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Wed Aug 4 15:57:08 2010 -0700
    
        Fix to various minor logic errors/typos related to moving over to
        entities.
    
    commit f7314e7b99946c5e5914b524bcdd9faeabfc27f3
    Author: Sam Boyer <tech@samboyer.org>
    Date:   Thu Jul 15 23:08:41 2010 -0700
    
        Incremental (very much non-working) commit towards using the new
        entities-based API.

--- includes/VersioncontrolGitRepository.php
+++ includes/VersioncontrolGitRepository.php
@@ -3,6 +3,17 @@
 class VersioncontrolGitRepository extends VersioncontrolRepository {
 
   /**
+   * State flag indicating whether or not the GIT_DIR variable has been pushed
+   * into the environment.
+   *
+   * Used to prevent multiple unnecessary calls to putenv(). Will be obsoleted
+   * by the introduction of a cligit library.
+   *
+   * @var bool
+   */
+  public $envSet = FALSE;
+
+  /**
    * Overwrite to get short sha-1's
    */
   public function formatRevisionIdentifier($revision, $format = 'full') {
@@ -17,4 +28,93 @@ class VersioncontrolGitRepository extends VersioncontrolRepository {
     }
   }
 
+  /**
+   * Ensure environment variables are set for interaction with the repository on
+   * disk.
+   *
+   * Hopefully temporary, until we can get a proper cligit library written.
+   */
+  public function setEnv() {
+    if (!$this->envSet) {
+      $root = escapeshellcmd($this->root);
+      putenv("GIT_DIR=$root/.git");
+      $this->envSet = TRUE;
+    }
+  }
+
+  /**
+   * Invoke git to fetch a list of local branches in the repository, including
+   * the SHA1 of the current branch tip and the branch name.
+   */
+  public function fetchBranches() {
+    $branches = array();
+
+    $data = array(
+      'repo_id' => $this->repo_id,
+      'action' => VERSIONCONTROL_ACTION_MODIFIED,
+      'label_id' => NULL,
+    );
+    $logs = $this->exec('git show-ref --heads');
+    while (($branchdata = next($logs)) !== FALSE) {
+      list($data['tip'], $data['name']) = explode(' ', trim($branchdata));
+      $data['name'] = substr($data['name'], 11);
+      $branches[$data['name']] = new VersioncontrolBranch($this->backend);
+      $branches[$data['name']]->build($data);
+    }
+
+    return $branches;
+  }
+
+  /**
+   * Invoke git to fetch a list of local tags in the repository, including
+   * the SHA1 of the commit to which the tag is attached.
+   */
+  public function fetchTags() {
+    $tags = array();
+    $data = array(
+      'repo_id' => $this->repo_id,
+      'action' => VERSIONCONTROL_ACTION_MODIFIED,
+      'label_id' => NULL,
+    );
+
+    $logs = $this->exec('git show-ref --tags');
+    while (($tagdata = next($logs)) !== FALSE) {
+      list($data['tip'], $data['name']) = explode(' ', trim($tagdata));
+      $data['name'] = substr($data['name'], 10);
+      $tags[$data['name']] = new VersioncontrolTag();
+      $tags[$data['name']]->build($data);
+    }
+
+    return $tags;
+  }
+
+  public function fetchCommits($branch_name = NULL) {
+    $logs = $this->exec('git rev-list ' . (empty($branch_name) ? '--all' : $branch_name));
+    $commits = array();
+    while (($line = next($logs)) !== FALSE) {
+      $commits[] = trim($line);
+    }
+    return $commits;
+  }
+
+  /**
+   * Execute a Git command using the root context and the command to be
+   * executed.
+   *
+   * @param string $command
+   *   Command to execute.
+   * @return mixed
+   *  Logged output from the command; an array of either strings or file
+   *  pointers.
+   */
+  protected function exec($cmds) {
+    if (!$this->envSet) {
+      $this->setEnv();
+    }
+    $logs = array();
+    exec($cmds, $logs);
+    array_unshift($logs, '');
+    reset($logs); // Reset the array pointer, so that we can use next().
+    return $logs;
+  }
 }
