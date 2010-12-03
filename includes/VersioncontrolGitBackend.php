557601274b7a6106a94d57363ecec51e33c5e925
Marco Villegas <marvil07@gmail.com>
2010-12-03 08:47:16 +0000

    task #979338: Overwrite revision field output using backend class.

--- includes/VersioncontrolGitBackend.php
+++ includes/VersioncontrolGitBackend.php
@@ -20,4 +20,19 @@ class VersioncontrolGitBackend extends VersioncontrolBackend {
     );
   }
 
+  /**
+   * Overwrite to get short sha-1's
+   */
+  public function formatRevisionIdentifier($revision, $format = 'full') {
+    switch ($format) {
+      case 'short':
+        // Let's return only the first 7 characters of the revision identifier,
+        // like git log --abbrev-commit does by default.
+        return substr($revision, 0, 7);
+      case 'full':
+      default:
+        return $revision;
+    }
+  }
+
 }
--- includes/VersioncontrolGitRepository.php
+++ includes/VersioncontrolGitRepository.php
@@ -14,21 +14,6 @@ class VersioncontrolGitRepository extends VersioncontrolRepository {
   public $envSet = FALSE;
 
   /**
-   * Overwrite to get short sha-1's
-   */
-  public function formatRevisionIdentifier($revision, $format = 'full') {
-    switch ($format) {
-    case 'short':
-      // Let's return only the first 7 characters of the revision identifier,
-      // like git log --abbrev-commit does by default.
-      return substr($revision, 0, 7);
-    case 'full':
-    default:
-      return $revision;
-    }
-  }
-
-  /**
    * Ensure environment variables are set for interaction with the repository on
    * disk.
    *
