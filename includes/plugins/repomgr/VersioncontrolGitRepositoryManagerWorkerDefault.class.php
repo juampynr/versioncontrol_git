2081d07f0bf910ebdece3d8150db366dc4dd8d48
Sam Boyer <drupal@samboyer.org>
2011-02-19 21:26:25 +0000

    Make VersioncontrolGitRepositoryManagerWorkerDefault NOT a stub.

--- includes/plugins/repomgr/VersioncontrolGitRepositoryManagerWorkerDefault.class.php
+++ includes/plugins/repomgr/VersioncontrolGitRepositoryManagerWorkerDefault.class.php
@@ -4,20 +4,82 @@ class VersioncontrolGitRepositoryManagerWorkerDefault implements VersioncontrolG
 
   protected $repository;
 
+  protected $templateDir;
+
   public function setRepository(VersioncontrolRepository $repository) {
     $this->repository = $repository;
   }
 
   public function init() {
-    ;
+    exec('mkdir -p ' . escapeshellarg($this->repository->root), $output, $return);
+    if ($return) {
+      // mkdir failed for some reason, throw exception
+      throw new Exception('Failed to mkdir for intended repository location "' . $this->repository->root . '"; mkdir exited with code ' . $return, E_ERROR);
+    }
+
+    // Create the repository on disk. Init with a template dir if one exists
+    if (!empty($this->templateDir) && file_exists($this->templateDir)) {
+      $return = $this->passthru('init --template ' . $template_dir, TRUE);
+    }
+    else {
+      $return = $this->passthru('init', TRUE);
+    }
+
+    if ($return) {
+      // init failed for some reason, throw exception
+      throw new Exception('Git repository initialization failed with code ' . $return, E_ERROR);
+    }
+
+    return TRUE;
   }
 
-  public function create() {
-    ;
+  public function reInit(array $flush) {
+    if (!empty($flush)) {
+      $flush = count($flush) > 1 ? '{' . implode(',', $flush) . '}' : array_shift($flush);
+      exec("rm -rf {$this->repository->root}/$flush");
+    }
+    $this->init();
+  }
+
+  public function configSet($name, $value, $type = NULL) {
+    $cmd = 'config --file ';
+    if (!is_null($type) && in_array($type, array('int', 'bool', 'path'))) {
+      $cmd .= "--$type ";
+    }
+    $cmd .= escapeshellarg($name) . ' ' . escapeshellarg($value);
+    $return = $this->passthru($cmd);
+    return empty($return);
   }
 
   public function delete() {
-    ;
+    exec('rm -rf ' . escapeshellarg($this->repository->root), $output, $return);
+    if ($return) {
+      // Deletion failed, throw an error.
+      throw new Exception('Git repository deletion failed with code ' . $return . ' and error message \'' . implode(' ', $output) . '\'', E_ERROR);
+    }
+
+    $this->repository->delete();
+    return TRUE;
+  }
+
+  public function save() {
+    $this->repository->save();
+    return TRUE;
+  }
+
+  public function setDescription($description) {
+    file_put_contents($this->repository->root . '/description', $description);
+    return TRUE;
+  }
+
+  public function passthru($command, $exception = FALSE) {
+    $prepend = "GIT_DIR={$this->repository->root} " . _versioncontrol_git_get_binary_path() . ' ';
+    $command = escapeshellcmd($prepend . $command);
+    exec($command, $output, $return);
+    if ($exception && $return) {
+      throw new Exception ("Command '$command' exited with status $return and the following output: " . implode(' ', $output), E_ERROR);
+    }
+    return $return;
   }
 }
 
