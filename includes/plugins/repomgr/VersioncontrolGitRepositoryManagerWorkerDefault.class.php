124d7add0eb6da89336459db3ae5293e78f81560
Sam Boyer <drupal@samboyer.org>
2011-02-21 03:10:53 +0000

    Seriously upgrade the intelligence of git repomgr workers with proc_open.

--- includes/plugins/repomgr/VersioncontrolGitRepositoryManagerWorkerDefault.class.php
+++ includes/plugins/repomgr/VersioncontrolGitRepositoryManagerWorkerDefault.class.php
@@ -16,18 +16,15 @@ class VersioncontrolGitRepositoryManagerWorkerDefault implements VersioncontrolG
   }
 
   public function init() {
-    exec('mkdir -p ' . escapeshellarg($this->repository->root), $output, $return);
-    if ($return) {
-      // mkdir failed for some reason, throw exception
-      throw new Exception('Failed to mkdir for intended repository location "' . $this->repository->root . '"; mkdir exited with code ' . $return, E_ERROR);
-    }
+    // if mkdir fails for some reason, it'll error out
+    $this->proc_open('mkdir -p ' . escapeshellarg($this->repository->root), TRUE, NULL, array());
 
     // Create the repository on disk. Init with a template dir if one exists
     if (!empty($this->templateDir) && file_exists($this->templateDir)) {
       $return = $this->passthru('init --template ' . $this->templateDir, TRUE);
     }
     else {
-      $return = $this->passthru('init', TRUE);
+      $return = $this->passthru('init', FALSE);
     }
 
     if ($return) {
@@ -41,7 +38,8 @@ class VersioncontrolGitRepositoryManagerWorkerDefault implements VersioncontrolG
   public function reInit(array $flush) {
     if (!empty($flush)) {
       $flush = count($flush) > 1 ? '{' . implode(',', $flush) . '}' : array_shift($flush);
-      exec("rm -rf {$this->repository->root}/$flush");
+      $command = "rm -rf " . escapeshellarg($this->repository->root) . "/$flush";
+
     }
     $this->init();
   }
@@ -57,25 +55,19 @@ class VersioncontrolGitRepositoryManagerWorkerDefault implements VersioncontrolG
   }
 
   public function delete() {
-    exec('rm -rf ' . escapeshellarg($this->repository->root), $output, $return);
-    if ($return) {
-      // Deletion failed, throw an error.
-      throw new Exception('Git repository deletion failed with code ' . $return . ' and error message \'' . implode(' ', $output) . '\'', E_ERROR);
-    }
+    $command = 'rm -rf ' . escapeshellarg($this->repository->root);
+    // This'll error out if deletion fails.
+    $this->proc_open($command, TRUE, NULL, array());
 
     $this->repository->delete();
     return TRUE;
   }
 
   public function move($target) {
-    exec('mv ' . escapeshellarg($this->repository->root) . ' ' . escapeshellarg($target), $output, $return);
-
-    if ($return) {
-      // move failed for some reason, throw exception
-      throw new Exception('Relocating Git repository on disk failed with code ' . $return . ' and error message \'' . implode(' ', $output) . '\'', E_ERROR);
-    }
-
+    $command = 'mv ' . escapeshellarg($this->repository->root) . ' ' . escapeshellarg($target);
+    $this->proc_open($command, TRUE, NULL, array());
     $this->repository->root = $target;
+    return TRUE;
   }
 
   public function save() {
@@ -89,13 +81,56 @@ class VersioncontrolGitRepositoryManagerWorkerDefault implements VersioncontrolG
   }
 
   public function passthru($command, $exception = FALSE) {
-    $prepend = "GIT_DIR={$this->repository->root} " . _versioncontrol_git_get_binary_path() . ' ';
-    $command = escapeshellcmd($prepend . $command);
-    exec($command, $output, $return);
-    if ($exception && $return) {
-      throw new Exception ("Command '$command' exited with status $return and the following output: " . implode(' ', $output), E_ERROR);
+    // $command = escapeshellcmd($prepend . $command);
+    $command = _versioncontrol_git_get_binary_path() . ' ' . $command;
+
+    $env = array(
+      'GIT_DIR' => $this->repository->root,
+    );
+    return $this->proc_open($command, $exception, NULL, $env);
+  }
+
+  /**
+   * Actually run the command, with full output control using the goodies of
+   * proc_open().
+   *
+   * This is fully generic, so can also be used for git commands - we do all the
+   * escaping and limiting to a particular dir in the public-facing passthru()
+   * method, which then calls this.
+   */
+  protected function proc_open($command, $exception, $cwd, $env) {
+    $descriptor_spec = array(
+      1 => array('pipe', 'w'),
+      2 => array('pipe', 'w'),
+    );
+
+    $process = proc_open($command, $descriptor_spec, $pipes, $cwd = NULL, $env = array());
+    if (is_resource($process)) {
+      $stdout = stream_get_contents($pipes[1]);
+      fclose($pipes[1]);
+      $stderr = stream_get_contents($pipes[2]);
+      fclose($pipes[2]);
+
+      $return_code = proc_close($process);
+
+      if ($return_code) {
+        $vars = array(
+          '%command' => $command,
+          '%code' => $return_code,
+          '%stdout' => $stdout,
+          '%stderr' => $stderr,
+        );
+        $text = "Invocation of '%command' exited with return code %code";
+        watchdog('vc_git', $text . ", emitting stdout:\n%stdout\n\nand stderr:\n%stderr", $vars, WATCHDOG_ERROR);
+        if ($exception) {
+          throw new Exception(strtr($text, array_slice($vars, 0, 2)), E_ERROR);
+        }
+      }
+
+      return $return_code;
     }
-    return $return;
+
+    return FALSE;
   }
 }
 
